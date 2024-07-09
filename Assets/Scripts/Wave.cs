22ee41eba25c0a0a6f191bce96ccb28801178ae8
Javier Rojas <javierito.rojas.ramirez@gmail.com>
2024-07-09 20:35:51 +0100

    Level 2: Added Sine Function Beta Level

--- Assets/Scripts/Wave.cs
+++ Assets/Scripts/Wave.cs
@@ -1,13 +1,16 @@
 // using System.Collections;
 // using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
-public class Oscillator : MonoBehaviour
+public class Wave : MonoBehaviour
 {
     Vector3 _startingPosition;
     [SerializeField] Vector3 movementVector;
     [SerializeField] private float period = 2f;
     [SerializeField] [Range(0, 2 * Mathf.PI)] private float phase = 0f;
+    [SerializeField] public float initialX = 0f;
+    [SerializeField] public float numberOfWaves = 1f;
 
     float _movementFactor;
     
@@ -25,7 +28,8 @@ public class Oscillator : MonoBehaviour
         float cycles = Time.time / period; // Continually growing over time
         
         const float tau = Mathf.PI * 2; // Constant value of 6.283
-        float rawSinWave = Mathf.Sin(cycles * tau + phase); // Going from -1 to 1
+        float xPos = transform.position.x - initialX;
+        float rawSinWave = Mathf.Sin(cycles * tau + numberOfWaves * xPos + phase); // Going from -1 to 1
 
         _movementFactor = (rawSinWave + 1f) / 2f; // Recalculated to go from 0 to 1, so it's cleaner
         
