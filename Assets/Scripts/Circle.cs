06b3bbd969d7da8306d069f589dbe7bb1656ccae
Javier Rojas <javierito.rojas.ramirez@gmail.com>
2024-07-10 22:50:25 +0100

    Over the Sine (Level N#3): Added Circles maze (BETA)

--- Assets/Scripts/Circle.cs
+++ Assets/Scripts/Circle.cs
@@ -3,37 +3,53 @@
 using UnityEngine;
 using UnityEngine.Serialization;
 
-public class Wave : MonoBehaviour
+public class Circle : MonoBehaviour
 {
     Vector3 _startingPosition;
-    [SerializeField] Vector3 movementVector;
+    [SerializeField] float magnitude = 10f;
     [SerializeField] private float period = 2f;
     [SerializeField] [Range(0, 2 * Mathf.PI)] private float phase = 0f;
-    [SerializeField] public float initialX = 0f;
-    [SerializeField] public float numberOfWaves = 1f;
-
+    [SerializeField] Vector2 centerPosition;
+    
+    Vector3 _movementVector;
     float _movementFactor;
+    private float _angle = 0f;
     
     // Start is called before the first frame update
     void Start()
     {
         _startingPosition = transform.position;
+
+        float xCat = transform.position.x - centerPosition.x;
+        float yCat = transform.position.y - centerPosition.y;
+
+        if (xCat != 0)
+        {
+            _angle = Mathf.Atan(yCat / xCat);
+            phase = phase + _angle;
+        }
+        else
+        {
+            _angle = Mathf.PI / 2;
+            phase = phase + _angle;
+        }
+
+        _movementVector = new Vector3(magnitude * Mathf.Cos(_angle), magnitude * Mathf.Sin(_angle), 0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (period <= Mathf.Epsilon) { return; }
+        if (period == 0f) { return; }
         
         float cycles = Time.time / period; // Continually growing over time
         
         const float tau = Mathf.PI * 2; // Constant value of 6.283
-        float xPos = transform.position.x - initialX; // Get the position of the Particle
-        float rawSinWave = Mathf.Sin(cycles * tau + numberOfWaves * xPos + phase); // Going from -1 to 1
+        float rawSinWave = Mathf.Sin(cycles * tau + phase); // Going from -1 to 1
 
         _movementFactor = (rawSinWave + 1f) / 2f; // Recalculated to go from 0 to 1, so it's cleaner
         
-        Vector3 offset = movementVector * _movementFactor;
+        Vector3 offset = _movementVector * _movementFactor;
         transform.position = _startingPosition + offset;
     }
 }
